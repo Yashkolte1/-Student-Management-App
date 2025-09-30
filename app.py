11549e3224a9a737b44687934870eca36bec21a7
Yashkolte1 <164024588+Yashkolte1@users.noreply.github.com>
2025-09-30 17:57:53 +0530

    app.py

--- app.py
+++ app.py
@@ -53,4 +53,6 @@ def delete_student(id):
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
